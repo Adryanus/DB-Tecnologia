f47dc3d159124476e9c98137becc25e78d8de410
Adryanus <adrianodb@gmail.com>
2025-11-18 21:12:07 -0300

    se agrega manejo de ordenes de compra

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import "./Cart.css";
 import { CartContext } from "../../context/CartContext/CartContext";
 import { useContext, useState } from "react";
-import { CheckoutForm } from "../CheckOutForm/CheckOutForm";
+import { CheckoutForm } from "../CheckoutForm/CheckoutForm";
 
 export const Cart = () => {
     const { cart, decreaseItem, addItem, deleteItem, clearCart, total, checkout } = useContext(CartContext);
